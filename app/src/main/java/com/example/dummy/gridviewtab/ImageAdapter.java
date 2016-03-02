ceb56242367251299ef6db88849c0f8ed160fbd7
Dummy <Dummy>
2016-03-02 07:08:02 -0700

    commit this shit

--- app/src/main/java/com/example/dummy/gridviewtab/ImageAdapter.java
+++ app/src/main/java/com/example/dummy/gridviewtab/ImageAdapter.java
@@ -1,14 +1,14 @@
 package com.example.dummy.gridviewtab;
 
 import android.content.Context;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Environment;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -30,14 +30,6 @@ public class ImageAdapter extends BaseAdapter {
             System.exit(-1);
         }*/
         images = new ArrayList<File>();
-    }
-    public int getCount() {
-        /*File[] filesInSelfit = f.listFiles();
-        for(int i = 0; i < filesInSelfit.length; i++) {
-            if(filesInSelfit[i].isFile() && filesInSelfit[i].getName().endsWith(".jpg")) {
-                images.add(filesInSelfit[i]);
-            }
-        }*/
         if(f.exists()) {
             String[] filesInSelfit = f.list();
             if(filesInSelfit != null) {
@@ -57,20 +49,49 @@ public class ImageAdapter extends BaseAdapter {
             System.err.println("directory doesn't exist");
             System.exit(-1);
         }
+    }
+    public int getCount() {
+        /*File[] filesInSelfit = f.listFiles();
+        for(int i = 0; i < filesInSelfit.length; i++) {
+            if(filesInSelfit[i].isFile() && filesInSelfit[i].getName().endsWith(".jpg")) {
+                images.add(filesInSelfit[i]);
+            }
+        }*/
+        /*images = new ArrayList<File>();
+        if(f.exists()) {
+            String[] filesInSelfit = f.list();
+            if(filesInSelfit != null) {
+                for(int i = 0; i < filesInSelfit.length; i++) {
+                    if(filesInSelfit[i].endsWith(".jpg")) {
+                        images.add(new File(f.getAbsolutePath() + "/" + filesInSelfit[i]));
+                    }
+                }
+            }
+            else {
+                System.err.println("list returning as null");
+                System.err.println(f.getAbsolutePath());
+                //images.add(new File(f.getAbsolutePath() + "/IMG_20160217_145507.jpg"));
+            }
+        }
+        else {
+            System.err.println("directory doesn't exist");
+            System.exit(-1);
+        }*/
         return images.size();
     }
     public View getView(int position, View convertView, ViewGroup parent) {
         ImageView imageView;
         if(convertView == null) {
             imageView = new ImageView(mContext);
-            imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
+            imageView.setLayoutParams(new GridView.LayoutParams(300, 300));
             imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
             imageView.setPadding(8, 8, 8, 8);
         }
         else {
             imageView = (ImageView) convertView;
         }
-        imageView.setImageURI(Uri.fromFile(images.get(position)));
+        Picasso.with(mContext).load(images.get(position)).fit().placeholder(R.drawable.white).into(imageView);
+        //imageView.setImageURI(Uri.fromFile(images.get(position)));
         //imageView.setImageBitmap(BitmapFactory.decodeFile(images.get(position).getAbsolutePath()));
         return imageView;
     }
